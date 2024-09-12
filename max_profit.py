87c0035eb9f466f47b6004b12ff90cdb05d07854
Classive <speecheq@gmail.com>
2024-09-12 13:20:19 +0900

    changed

--- max_profit.py
+++ max_profit.py
@@ -4,6 +4,9 @@ def maxProfit_bruteforce (prices):
    for i, price in enumerate(prices):
        for j in range(i, len(prices)):
            max_price = max(prices[j] - price, max_price)
+def minPorfit_bruteforce (prices):
+    min_price = -1
+    for i in range(price)
 
    return
 
